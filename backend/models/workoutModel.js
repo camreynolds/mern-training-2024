86e4b41e017387b35605feb08b6a6f3ee3b66435
Carlos Muñoz <camr.pesonal@gmail.com>
2024-01-04 15:24:13 -0500

    part 5. Model & Schema

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const workoutSchema = Schema({
+  title:{
+    type: String,
+    require: true,
+  },
+  reps:{
+    type: Number,
+    require: true
+  },
+  load:{
+    type: Number,
+    require: true
+  }
+},{timestamps: true})
+
+module.exports = mongoose.model("Workout", workoutSchema)
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const Workout = require("../models/workoutModel")
 
 router.get("/", (req,res) =>{
   res.status(200).json({mssg: "get all the workouts."})
@@ -9,8 +10,14 @@ router.get("/:id", (req,res) =>{
   res.status(200).json({mssg: "get a single workout."})
 })
 
-router.post("/", (req,res) =>{
-  res.status(200).json({mssg: "create a single workout."})
+router.post("/", async (req,res) =>{
+  const {title,reps,load} = req.body
+  try {
+    const workout = await Workout.create({title,reps,load})
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 })
 
 router.patch("/", (req,res) =>{
