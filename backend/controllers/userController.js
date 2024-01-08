8061c027fee98acb3a35a6f5cea19a6783fa9af7
Carlos Muñoz <camr.pesonal@gmail.com>
2024-01-08 15:52:56 -0500

    part 7. user backend done.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,32 +1,32 @@
+require("dotenv").config()
 const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
+
+const createToken = (_id) =>{
+  return jwt.sign({_id},process.env.SECRET, {expiresIn: "10m"})
+}
 
 const login = async (req,res) =>{
   const {email,password} = req.body
 
-  if(!email || !password){
-    return res.status(400).json({error: "all the fileds must be fill in."})
-  }
-
  try{
-  const user = await User.create({email,password})
-  res.status(200).json(user)
+  const user = await User.login(email,password)
+  const token = createToken(user._id)
+  res.status(200).json({email,token})
  }catch(error){
-  res.status(400).json({error:"user couldn't be logged in."})
+  res.status(400).json({error: error.message})
  }
 }
 
 const signup = async (req,res) =>{
   const {email,password} = req.body
 
-  if(!email || !password){
-    return res.status(400).json({errro: "all the fields must be fill in."})
-  }
-
   try{
-    const user = await User.create({email,password})
-    res.status(200).json(user)
+    const user = await User.signup(email,password)
+    const token = createToken(user._id)
+    res.status(200).json({email,token})
   }catch(error){
-    res.status(400).json({error: "user couldn't be signed in."})
+    res.status(400).json({error: error.message})
   }
 }
 
