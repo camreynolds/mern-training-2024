0781b599a3f1f39e9215008dcf0c5f142987da74
Carlos Muñoz <camr.pesonal@gmail.com>
2024-01-10 17:17:58 -0500

    part user 13. check user in authContext.

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import {createContext,useReducer} from "react"
+import { useEffect } from "react"
 
 export const AuthContext = createContext()
 
@@ -23,6 +24,14 @@ export const AuthContextProvider = ({children}) =>{
   const [state,dispatch] = useReducer(authReducer,{
     user:null
   })
+
+  useEffect( () =>{
+    const user = JSON.parse(localStorage.getItem("user"))
+
+    if(user){
+      dispatch({type: "LOGIN", payload:user})
+    }
+  },[])
 
   console.log("AuthContext:", state)
   
