3be172eaecfbaf268c9c25770b90f2abb448a1d5
Carlos Muñoz <camr.personal@gmail.com>
2024-01-14 18:19:20 -0500

    from the begining.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 require("dotenv").config()
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 
-// app.use(express.json())
-// app.use((req,res,next) =>{
-//     console.log(req.path,req.method)
-//     next
-// })
-
-app.listen(process.env.PORT, () =>{
-    console.log("server listening on port:",process.env.PORT);
+app.use(express.json())
+app.use((req,res,next) =>{
+    console.log(req.path,req.method)
+    next()
 })
+
+app.get("/", (req,res) =>{
+	res.status(200).json("Welcome to the app.")
+})
+
+mongoose.connect(process.env.MONGO_DB)
+	.then( () => {
+		app.listen(process.env.PORT, () =>{
+				console.log("conneted to server & database listening on port:",process.env.PORT);
+		})
+	})
+	.catch(error =>{
+		console.log(error);
+	})
