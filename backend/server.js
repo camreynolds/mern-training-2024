186f663b5ebbb5b3c2133a352f428b2854db4bf2
Carlos Muñoz <camr.personal@gmail.com>
2024-01-14 16:07:57 -0500

    backend init.

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,13 @@
 require("dotenv").config()
 const express = require("express")
-const mongoose = require("mongoose")
 const app = express()
-const workoutsRoutes = require("./routes/workouts")
-const userRoutes = require("./routes/user")
 
-// middleware
-app.use( (req,res,next) => {
-  console.log(req.method, req.path)
-  next()
+// app.use(express.json())
+// app.use((req,res,next) =>{
+//     console.log(req.path,req.method)
+//     next
+// })
+
+app.listen(process.env.PORT, () =>{
+    console.log("server listening on port:",process.env.PORT);
 })
-app.use(express.json())
-
-//routes
-app.use("/api/workouts", workoutsRoutes)
-app.use("/api/users", userRoutes)
-
-
-//connection to the database
-mongoose.connect(process.env.DATABASE_URI)
-  .then( () => {
-    // listening for request
-    app.listen(process.env.PORT_SERVER, () => {
-      console.log("Connecting to DB & listening on port:",process.env.PORT_SERVER); 
-     })
-  })
-  .catch( error => {
-    console.log(error);
-  })
-
