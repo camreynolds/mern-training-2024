a995e8d375f0c4c435451e82f909e5294c90d17e
Carlos Muñoz <camr.pesonal@gmail.com>
2024-01-22 09:59:35 -0500

    Backend begins.

--- backend/routes/usersRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const usersRoutes = express.Router()
-const {usersLogin,usersSignup} = require("../controllers/usersControllers")
-
-usersRoutes.post("/login", usersLogin)
-usersRoutes.post("/signup", usersSignup)
-
-module.exports = usersRoutes
