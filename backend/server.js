5e0b0122ef6f27889cc9c08bed7813a5c29773a0
Carlos Muñoz <camr.pesonal@gmail.com>
2024-01-22 14:02:29 -0500

    user backend.

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const app = express()
-const workoutsRoutes = require("./routes/workoutsRoutes")
+
 
 app.use(express.json())
 app.use( (req,res,next) =>{
   console.log(req.path, req.method)
   next()
 })
-app.use("/api/workouts", workoutsRoutes)
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(
