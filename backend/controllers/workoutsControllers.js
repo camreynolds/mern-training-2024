f13b9c529758f529a429807d6a1be397f581b9d9
Carlos Muñoz <camr.pesonal@gmail.com>
2024-01-24 12:02:53 -0500

    backends.

--- backend/controllers/workoutsControllers.js
+++ backend/controllers/workoutsControllers.js
@@ -1,21 +1,89 @@
-const getAllWorkouts = (req,res) =>{
-  res.status(200).json({mssg: "get all the workouts."})
+const Workout = require("../model/workoutModel")
+const mongoose = require("mongoose")
+
+const getAllWorkouts = async (req,res) =>{
+  try{
+    const workouts = await Workout.find({}).sort({createdAt: -1})
+    res.status(200).json(workouts)
+  }catch(error){
+    res.status(400).json({error: "not such workouts"})
+  }
 }
 
-const getSingleWorkout = (req,res) =>{
-  res.status(200).json({mssg: "get a single workout."})
+const getSingleWorkout = async (req,res) =>{
+  const {_id} = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(_id)){
+    return res.status(400).json({error: "this is not a valid id."})
+  }
+
+  const workout = await Workout.findById({_id})
+
+  if(!workout){
+    return res.status(400).json({error: "not such workout"})
+  }
+
+  res.status(200).json(workout)
 }
 
-const createSingleWorkout = (req,res) =>{
-  res.status(200).json({mssg: "create single workout."})
+const createSingleWorkout = async (req,res) =>{
+  const {title,load,reps} = req.body
+  const isEmpty = []
+
+  if(!title){
+    isEmpty.push("title")
+  }
+
+  if(!load){
+    isEmpty.push("load")
+  }
+
+  if(!reps){
+    isEmpty.push("reps")
+  }
+
+  if(isEmpty.length > 0){
+    return res.status(400).json({error: "all the fields must be fill in.",isEmpty})
+  }
+
+  try{
+    const workout = await Workout.create({title,load,reps})
+    res.status(200).json(workout)
+  }catch(error){
+    res.status(400).json({error: "workout couldn't be created."})
+  }
 }
 
-const updateSingleWorkout = (req,res) => {
-  res.status(200).json({mssg: "update singel workout."})
+const updateSingleWorkout = async (req,res) => {
+  const {_id} = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(_id)){
+    return res.status(400).json({error: "this is not a valid id."})
+  }
+
+  const workout = await Workout.findByIdAndUpdate({_id},{...req.body})
+
+  if(!workout){
+    return res.status(400).json({error: "not such workout."})
+  }
+
+  res.status(200).json(workout)
 }
 
-const deleteSingleWorkout = (req,res) => {
-  res.status(200).json({mssg: "delete single workout"})
+const deleteSingleWorkout = async (req,res) => {
+  const {_id} = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(_id)){
+    return res.status(400).json({errro: "this is not a vaid id."})
+  }
+
+  const workout = await Workout.findByIdAndDelete({_id})
+
+  if(!workout){
+    return res.status(400).json({error: "not such workout."})
+  }
+
+  res.status(200).json(workout)
 }
 
 module.exports = {getAllWorkouts,getSingleWorkout,createSingleWorkout,updateSingleWorkout,deleteSingleWorkout}
