7cdb50c259954da1a867d26a8b711a698e0f6ceb
Carlos Muñoz <camr.pesonal@gmail.com>
2024-01-30 20:33:27 -0500

    css done

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,25 +11,27 @@ const Navbar = () =>{
   }
 
   return(
-    <div className="container">
-      <Link to="/">
-        <h1>Workout Buddy</h1>
-      </Link>
-      <nav>
-        {user && (
-          <div>
-            {user.email}
-            <button onClick={handleClick}>logout</button>
-          </div>
-        )}
-        {!user && (
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-          </div>
-        )}
-      </nav>
-    </div>
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>logout</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
+        </nav>
+      </div>  
+    </header>
   )
 }
 
